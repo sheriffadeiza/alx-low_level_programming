aef0d8a272f94c03eae39cb5788b8c4cc3946a76
sheriffadeiza <sheriffadeiza76@gmail.com>
2023-07-14 12:06:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry points
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
