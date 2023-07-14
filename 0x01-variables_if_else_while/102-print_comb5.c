92454c2f235fb8e5bfcf93fd82edcd2732f7ce0c
sheriffadeiza <sheriffadeiza76@gmail.com>
2023-07-14 15:50:42 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,10 +13,10 @@ for (p = 0; p <= 98; p++)
 for (q = p + 1; q <= 99; q++)
 {
 putchar((p / 10) + '0');
-putchar((p % 10) + '0';
+putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
-putchar((q % 10) + '0';
+putchar((q % 10) + '0');
 if (p == 98 && q == 99)
 continue;
 putchar(',');
