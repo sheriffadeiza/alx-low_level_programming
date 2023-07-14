f28550ed0db773f3ce9687e9da6b73c0fdfce23e
sheriffadeiza <sheriffadeiza76@gmail.com>
2023-07-14 13:16:39 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,19 +8,15 @@
  */
 
 int main(void)
-
 {
-        char low, e, q;
-
-	e = 'e';
-	q = 'q';
-
-        for (low = 'a'; low <= 'z'; low++)
-	
-	{
-	if (low != e && low != q)
-        putchar(low);
-	}
-        putchar('\n');
-	return (0);
+char low, e, q;
+e = 'e';
+q = 'q';
+for (low = 'a'; low <= 'z'; low++)
+{
+if (low != e && low != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
 }
