d1b1c0599a1302cfe316cd96f449e1b8bf6ddb64
sheriff adeiza <sheriffadeiza76@gmail.com>
2023-08-29 15:48:29 -0700

    few linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements (x) of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->x);
+		x++;
+		h = h->next;
+	}
+
+	return (x);
+}
