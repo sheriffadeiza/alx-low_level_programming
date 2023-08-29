94f5d0e14247f09722053b5571626319d15d46f8
sheriff adeiza <sheriffadeiza76@gmail.com>
2023-08-29 16:48:30 -0700

    few linked lisk

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number  of elements in a linked lists
+ * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t x = 0;
+	size_t num = 0;
 
 	while (h)
 	{
@@ -16,4 +16,5 @@ size_t listint_len(const listint_t *h)
 		h = h->next;
 	}
 
-	return(num)
+	return (num);
+}
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int x)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int x)
 	if (!new)
 		return (NULL);
 
-	new->x = x;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 
